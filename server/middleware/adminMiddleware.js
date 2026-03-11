8ce2c3bdbe02aced77dea46561d724b3df9e67f5
Ayushm74 <Ayushmishra74992@gmail.com>
2026-03-12 00:09:08 +0530

    update

--- server/middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-export const adminOnly = (req, res, next) => {
-  if (!req.user || req.user.role !== "admin") {
-    return res
-      .status(403)
-      .json({ message: "Admin access required for this action" });
-  }
-  next();
-};
-
